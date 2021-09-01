4c81d30d3eccdd6c1902faf150b654b067422126
Jemimah <jpfilarca@gmail.com>
2021-09-01 23:58:46 +0200

    added post component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,13 @@
 import "./feed.css"
+import Share from "../../components/share/Share";
 
 
 export default function Feed(){
   return(
-    <div className="feedContainer">
-        Feed
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share/>
+      </div>
     </div>
    )
   }
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,10 @@
+import "./share.css"
+
+
+export default function Share(){
+  return(
+    <div className="share">
+        Share Component
+    </div>
+   )
+  }

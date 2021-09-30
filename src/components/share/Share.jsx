449769e71b86abb56ca9d0655a9c7784ba6f337b
Jemimah <jpfilarca@gmail.com>
2021-09-30 15:48:35 +0200

    added Share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,10 +1,25 @@
 import "./share.css"
-
+import {PermMedia} from "@material-ui/icons"
 
 export default function Share(){
   return(
     <div className="share">
-        Share Component
+        <div className="shareWrapper">
+          <div className="shareTop">
+             <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <input className="shareInput" placeholder="What's in your mind?" />
+          </div>
+          <hr className="shareHr"></hr>
+          <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <PermMedia className="shareIcon"/>
+                    <span className="shareOptionText">Photo or Video</span>
+                </div>
+            </div>
+          </div>
+
+        </div>
     </div>
    )
   }

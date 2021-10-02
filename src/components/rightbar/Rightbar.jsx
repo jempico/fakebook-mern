8f159050a539f8a31a17e6cd5e2ebe36c5cf261b
Jemimah <jpfilarca@gmail.com>
2021-10-03 01:33:03 +0200

    finished Rightbar component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,51 @@ import "./rightbar.css"
 export default function Rightbar(){
   return(
     <div className="rightbarContainer">
-        Rightbar
+        <div className="rightbarWrapper">
+            <div className="birthdayContainer">
+              <img src="/assets/gift.png" alt="" className="birthdayImg" />
+              <span className="birthdayText"> Paola Sen and 3 other friends have a birthday today.</span>
+            </div>
+            <img src="/assets/ad.png" alt="" className="rightbarAd" />
+            <h4 className="rightbarTitle">Online Friends</h4>
+            <ul className="rightbarFriendList">
+              <li className="rightbarFriend">
+                <div className="rightbarImgProfileContainer">
+                  <img src="/assets/person/3.jpeg" alt="" className="rightbarImgProfile" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">John Carter</span>
+              </li>
+              <li className="rightbarFriend">
+                <div className="rightbarImgProfileContainer">
+                  <img src="/assets/person/3.jpeg" alt="" className="rightbarImgProfile" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">John Carter</span>
+              </li>
+              <li className="rightbarFriend">
+                <div className="rightbarImgProfileContainer">
+                  <img src="/assets/person/3.jpeg" alt="" className="rightbarImgProfile" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">John Carter</span>
+              </li>
+              <li className="rightbarFriend">
+                <div className="rightbarImgProfileContainer">
+                  <img src="/assets/person/3.jpeg" alt="" className="rightbarImgProfile" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">John Carter</span>
+              </li>
+              <li className="rightbarFriend">
+                <div className="rightbarImgProfileContainer">
+                  <img src="/assets/person/3.jpeg" alt="" className="rightbarImgProfile" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">John Carter</span>
+              </li>
+            </ul>
+        </div>
     </div>
    )
   }

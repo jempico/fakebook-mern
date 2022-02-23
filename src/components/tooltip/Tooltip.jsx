5948abce90b527644b274b091e03ce5876ee76b9
Jemimah <jpfilarca@gmail.com>
2022-02-23 14:03:50 +0100

    added tooltip component

---
+++ src/components/tooltip/Tooltip.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./tooltip.css";
+
+export default function Tooltip(props){
+  let timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.delay || 400);
+  };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className="Tooltip-Wrapper"
+      // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {/* Wrapping */}
+      {props.children}
+      {active && (
+        <div className={`Tooltip-Tip ${props.direction || "top"}`}>
+          {/* Content */}
+          {props.content}
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,14 @@
 import "./topbar.css"
 import { Search, Person, Chat, Notifications } from "@mui/icons-material/"
 import {useContext} from "react"
-import {AuthContext} from "../../context/AuthContext";
+import {AuthContext} from "../../context/AuthContext"
 import logoutService from '../../services/logout'
+import Tooltip from "../tooltip/Tooltip"
 
 export default function Topbar() {
 
     // eslint-disable-next-line no-unused-vars
     const {user, isFetching, error, dispatch } = useContext(AuthContext);
-
 
     const handleLogOut = async() => {
         try {
@@ -31,24 +31,32 @@ export default function Topbar() {
             </div>
             <div className="topbarRight">
                 <div className="topbarLinks">
-                    <span className="topbarLink">Profile</span>
                     <span className="topbarLink" onClick={handleLogOut}>Logout</span>
                 </div>
                 <div className="topbarIcons">
+
+                <Tooltip content="Friend requests" direction="bottom">
                     <div className="topbarIconItem">
                         <Person />
                         <span className="tobarIconBadge">1</span>
                     </div>
+                </Tooltip>
+                <Tooltip content="Messenger" direction="bottom">
                     <div className="topbarIconItem">
                         <Chat />
                         <span className="tobarIconBadge">2</span>
                     </div>
+                </Tooltip>
+                <Tooltip content="Notifications" direction="bottom">
                     <div className="topbarIconItem">
                         <Notifications />
                         <span className="tobarIconBadge">3</span>
                     </div>
-                </div>
-            <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                </Tooltip>
+            </div>
+                <Tooltip content="Account" direction="bottom">
+                    <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                </Tooltip>
             </div>
         </div>
     )
